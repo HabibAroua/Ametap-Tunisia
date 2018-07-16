2d8196b9877d9d1e98a1c63995deb3b8f545c1cb
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-07-16 22:08:25 +0100

    TypeActiviteController

--- AMETAP/AMETAP/Controller/TypeActiviteController.cs
+++ AMETAP/AMETAP/Controller/TypeActiviteController.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AMETAP.Model.Business;
-using AMETAP.Model.DataAcces;
-using System.Windows.Forms;
-using MetroFramework.Forms;
+
 namespace AMETAP.Controller
 {
     class TypeActiviteController
     {
-        //TypeActiviteDA taDA;
         public TypeActiviteController()
-        {
-          //  taDA = new TypeActiviteDA();
-        }
-
+        {}
 
         public List<TypeActivite> ListActiviteInComboBox()
         {
-            //List<TypeActivite>  list = taDA.SelectAll();
-            return null; //list;
+            return null;
         }
 
     }
