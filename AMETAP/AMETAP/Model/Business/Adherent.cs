7e713e1f93f29417d7b2aef2a9d7a057a4f1d6bd
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-07-16 22:10:38 +0100

    Edit class Adherent

--- AMETAP/AMETAP/Model/Business/Adherent.cs
+++ AMETAP/AMETAP/Model/Business/Adherent.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.OleDb;
-
 
 namespace AMETAP.Model.Business
 {
     public class Adherent
     {
-        //Implement attribut
 
         public Personnel personnel
         {
@@ -46,7 +39,7 @@ namespace AMETAP.Model.Business
             get;
             set;
         }
-        //Constructeur
+        
         public Adherent(int matriculeAmetap, int matriculeEtap,String login,String password)
         {
             this.matriculeAmetap = matriculeAmetap;
