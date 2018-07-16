27a69e7863d30f8a77f0f6bc3a9ca4dbee4700f9
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-07-16 21:55:37 +0100

    Edit class ClubController

--- AMETAP/AMETAP/Controller/ClubController.cs
+++ AMETAP/AMETAP/Controller/ClubController.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AMETAP.Model.Business;
 using AMETAP.Model.DataAcces;
 using System.Windows.Forms;
