eb0734add184ab75d5c3db22f7231f6308127927
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-07-19 13:18:14 +0100

    Edit class Organisateur

--- AMETAP/AMETAP/Model/Business/Organisateur.cs
+++ AMETAP/AMETAP/Model/Business/Organisateur.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AMETAP.Model.Business
 {
     public class Organisateur
     {
-        //Implemment attribut
+        
         public int id
         {
             get;
@@ -30,10 +26,10 @@ namespace AMETAP.Model.Business
             get;
             set;
         }
-        //constructeur sans paramétre 
+         
         public Organisateur()
         { }
-        //Constructeur 
+         
         public Organisateur(int id, String nom_organisateur,String email,String adresse)
         {
             this.id = id;
