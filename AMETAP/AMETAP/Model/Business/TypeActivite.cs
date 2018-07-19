5e909bb40c1afb25aba9054018dfc05fb6502d0b
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-07-19 13:22:27 +0100

    edit class TypeActivite

--- AMETAP/AMETAP/Model/Business/TypeActivite.cs
+++ AMETAP/AMETAP/Model/Business/TypeActivite.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AMETAP.Model.Business
 {
@@ -21,9 +17,7 @@ namespace AMETAP.Model.Business
         }
 
         public TypeActivite()
-        {
-
-        }
+        {}
 
         public TypeActivite(int id,String libelle)
         {
